32fd71bfb3eca90138e0ec4e8c3a928fe7bbce17
HappyGnome <bencorp20002000@yahoo.co.uk>
2019-02-12 09:45:59 +0000

    Added author display for propositions. Changed GenerateRuleBox to options-object model. Changed dereferences of objects from json from["X"] to .X

--- docs/GenerateRuleBox.js
+++ docs/GenerateRuleBox.js
@@ -1,42 +1,44 @@
 function ToggleNotes(ruleID){
 			$("#"+ruleID+" .rulefooter").toggle();
 }
-function GenerateRuleBox(rules,rule_key,id_prefix="rule", toggle_notes=true, show_notes=false, rule_lookup_url="", rule_name_lookup=null, details=true){
+function GenerateRuleBox(rules,rule_key,options={id_prefix:"rule", toggle_notes:true, show_notes:false, rule_lookup_url:"", rule_name_lookup:null, details:true, show_author:false} ){
 	var rule=rules[rule_key];
-	var S="<section class=\"rulebox\" id=\""+id_prefix+rule_key+"\">"+
+	var S="<section class=\"rulebox\" id=\""+options.id_prefix+rule_key+"\">"+
 			"<div class=\"rulehead\"><h1>"+
 				rule.label+"</h1>";
-				if(details)S=S+"<div class=\"detail_link\">[<a href=rule_details.html?"
+				if(options.details)S+="<div class=\"detail_link\">[<a href=rule_details.html?"
 		+rule_key+">details</a>]</div>";
-		S=S+"<div class=\"subhead\">"+rule.date+"</div></div><div class=\"rulebody\" ";
-		if(toggle_notes) S=S+"onclick=\"ToggleNotes('"+id_prefix+rule_key+"')\"";
-		S=S+">";
+		S+="<div class=\"subhead\">";
+		if(options.show_author) S+=" - "+rule.author+" - ";		
+		S+=rule.date+"</div></div><div class=\"rulebody\" ";
+		if(options.toggle_notes) S+="onclick=\"ToggleNotes('"+options.id_prefix+rule_key+"')\"";
+		S+=">";
 		for (var i=0; i<rule.text.length; i++)
 		{
-			S=S+"<p>"+rule.text[i]+"</p>";
+			S+="<p>"+rule.text[i]+"</p>";
 		}
-		S=S+"</div>";
+		S+="</div>";
 		if(rule.notes.length || rule.linksto.length){
-			S=S+"<div class=\"rulefooter\"";
-			if(!show_notes)S=S+"style=\"display:none;\"";
-			S=S+">";
+			S+="<div class=\"rulefooter\"";
+			if(!options.show_notes)S+="style=\"display:none;\"";
+			S+=">";
 			if(rule.linksto.length){//C.F.		
 				S+="<div class=\"cf\"> <i>c.f. rules:</i>";
 				for (var j=rule.linksto.length-1; j>=0; --j){
 					var ind=rule.linksto[j];
 					var label=""
-					if(rule_name_lookup){//look in external rule list if given
-						label=rule_name_lookup[ind].label;
+					if(options.rule_name_lookup){//look in external rule list if given
+						label=options.rule_name_lookup[ind].label;
 					}
 					else label=rules[ind].label;
-					S+="<a href=\""+rule_lookup_url+"#rule"+ind+"\">"+label+"</a> "
+					S+="<a href=\""+options.rule_lookup_url+"#rule"+ind+"\">"+label+"</a> "
 				}
 				S+="</div>";
 			}						
-			S=S+"<ul>";//Notes
+			S+="<ul>";//Notes
 			for (var j=rule.notes.length-1; j>=0; --j){
 				var note=rule.notes[j];
-				S=S+"<li class=\"footnote\">"+note.content+"<span class=attrib> - "+note.author+" "+note.date+"</span></li>";
+				S+="<li class=\"footnote\">"+note.content+"<span class=attrib> - "+note.author+" "+note.date+"</span></li>";
 			}
 			S+="</ul></div>";
 		}
