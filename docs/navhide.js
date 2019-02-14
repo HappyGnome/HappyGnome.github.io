e458fbad1a88ea667a79e31c3f4dd6c5c7d49c71
HappyGnome <ben.c.pooley@gmail.com>
2019-02-14 20:17:03 +0000

    Style fixes. Moved JS back to body. Fixed typos in some entries.

--- docs/navhide.js
+++ docs/navhide.js
@@ -2,6 +2,6 @@ var prev_scroll=0;
 $(window).scroll(function(){
 	var new_scroll=$('html, body').scrollTop();
 	if (new_scroll<window.prev_scroll) $("nav").css("top","0px");
-	else $("nav").css("top","-30px");
+	else $("nav").css("top","-20px");
 	window.prev_scroll=new_scroll;
 });
